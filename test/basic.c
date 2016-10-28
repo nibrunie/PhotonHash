71f7705e1aec906df779391ca27622d37fa7d3bf
Nicolas Brunie <nibrunie@gmail.com>
2016-10-28 17:43:49 +0200

    Extending testing (removal, checks)

--- test/basic.c
+++ test/basic.c
@@ -14,19 +14,27 @@ int main(void) {
   printf("initializing photon map\n");
   photon_map_init(map, 16, 32); 
 
-  long excluded_key = rand();
+  long excluded_key = rand() % 256;
 
   long keys[RECORD_NUM]; // = {17, 42, 1337};
   long values[RECORD_NUM]; // = {171, 421, 13371};
+  long valid_key = excluded_key + 1;
   for (int i = 0; i < RECORD_NUM; ++i) {
-    while ((keys[i] = rand()) == excluded_key) {};
-    values[i] = rand();
+    // increment to ensure unicity
+    keys[i] = valid_key++;
+    values[i] = rand() % 256;
   }
 
   printf("inserting %d records into map\n", RECORD_NUM);
   for (int i = 0; i < RECORD_NUM; ++i) {
     int insert_r = photon_map_insert(map, keys[i], values[i]);
+    if (!insert_r) return -1;
   };
+
+  printf("testing insertion of overflowing record\n");
+  int insert_overflow = photon_map_insert(map, valid_key + 1, rand());
+  if (insert_overflow) return -1;
+
   printf("retrieving records from map\n");
   for (int i = RECORD_NUM - 1; i >= 0; i--) {
     long value = (long) photon_map_get(map, keys[i]);
@@ -44,6 +52,36 @@ int main(void) {
   int contain_excluded = photon_map_contains(map, excluded_key);
   printf("map contains excluded_key=%d\n", contain_excluded);
   if (contain_excluded) return -1;
+
+  printf("pre-removal map: \n");
+  photon_map_dump(map);
+
+  printf("testing removal\n");
+  for (int i = 0; i < RECORD_NUM; i += 4) {
+    int pre_contain_r = photon_map_contains(map, keys[i]);
+    int remove_r = photon_map_remove(map, keys[i]);
+    int contain_r = photon_map_contains(map, keys[i]);
+    printf("keys[%03d] removal, pre_contain_r = %d, remove_r = %d, post contain_r = %d\n", i, pre_contain_r, remove_r, contain_r);
+    if (!remove_r || contain_r) return -1;
+
+  }
+  printf("post-removal map: \n");
+  photon_map_dump(map);
+
+  printf("testing insertion post removal\n");
+  for (int i = 0; i < RECORD_NUM; i += 4) {
+    int insert_r = photon_map_insert(map, 2 * keys[i], 2 * values[i]);
+    if (!insert_r) return -1;
+  }
+  printf("testing contains post removal\n");
+  for (int i = RECORD_NUM - 1; i >= 0; i--) {
+    long key = (i % 4) ? keys[i] : (2 * keys[i]); 
+    int contain = photon_map_contains(map, key);
+    printf("map contains keys[%d] = %ld: %d\n", i, key, contain);
+    if (!contain) {
+      return -1;
+    }
+  }
 
 
 
